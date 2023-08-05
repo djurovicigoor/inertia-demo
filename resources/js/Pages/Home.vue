9abdc7dac2e2b7c5523acad75f1980d63dfca796
Djurović Igor <djurovic.igoor@gmail.com>
2023-08-05 16:53:16 +0200

    Pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+defineProps({
+	name: String,
+	frameworks: Array
+});
+</script>
+
+<template>
+	<div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+		<div class="max-w-7xl mx-auto p-6 lg:p-8">
+			<div class="mt-16">
+				<div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+					<div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+						<div class="text-gray-900 dark:text-white">
+							<h2 class="text-xl font-semibold">Hello, {{ name }}!</h2>
+							<p class="mt-4">In this series, we will use the following frameworks:</p>
+							<u class="mt-2">
+								<li v-for="framework in frameworks" v-text="framework"></li>
+							</u>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+		</div>
+	</div>
+</template>
+
+<style>
+.bg-dots-darker {
+	background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+}
+
+@media (prefers-color-scheme: dark) {
+	.dark\:bg-dots-lighter {
+		background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
+	}
+}
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -16,11 +15,11 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+    return Inertia::render('Home', [
+        'name' => 'John Doe',
+        'frameworks' => [
+            'Laravel', 'Vue', 'Inertia'
+        ]
     ]);
 });
 
