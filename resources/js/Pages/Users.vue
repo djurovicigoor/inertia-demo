11e14395d96cb52b0572f5e86378af43db0ae0f3
Djurović Igor <djurovic.igoor@gmail.com>
2023-08-07 11:56:16 +0200

    map data before send to the frontend

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,13 +1,18 @@
-<script>
+<script setup>
 import Layout from "@/Shared/Layout.vue";
 
-export default {layout: Layout};
+defineProps({
+	users: Array
+})
 </script>
 <template>
 	<Layout>
 		<h2 class="text-xl font-semibold">Users view</h2>
 	</Layout>
-
+	
+	<ul>
+		<li v-for="user in users" :key="user.id" v-text="user.name"></li>
+	</ul>
 </template>
 
 <style scoped>
